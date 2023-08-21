46c520b24be597116f28ab357a5af8791109c701
alpha-beta-eta <95096031+alpha-beta-eta@users.noreply.github.com>
2023-08-21 12:43:39 +0800

    Update examples.rkt

--- examples.rkt
+++ examples.rkt
@@ -1,79 +1,67 @@
 #lang racket
 (provide (all-defined-out))
 (require "mathml.rkt")
-(define binomial-identity
-  (: (^ (par0 (: :x :+ :y)) :n) :=
-     (sum (: :k := :0) :n
-          (: (comb :n :k) i*
-             (^ :x (: :n :- :k)) i*
-             (^ :y :k)))))
-(define det3-identity
-  (deriv
-   (det `((,:a ,:b ,:c)
-          (,:d ,:e ,:f)
-          (,:g ,:h ,:i)))
-   (: :a i* (det `((,:e ,:f)
-                   (,:h ,:i)))
-      :-
-      :b i* (det `((,:d ,:f)
-                   (,:g ,:i)))
-      :+
-      :c i* (det `((,:d ,:e)
-                   (,:g ,:h))))
-   (: :a i* :e i* :i :+
-      :b i* :f i* :g :+
-      :c i* :d i* :h :-
-      :c i* :e i* :g :-
-      :a i* :f i* :h :-
-      :b i* :d i* :i)))
-(define combinatorial-identity
-  (let ((m-1 (: :m :- :1))
-        (n-1 (: :n :- :1)))
-    (: (comb :m :n) :=
-       (comb m-1 n-1) :+
-       (comb m-1 :n))))
-(define computing-e
-  (: (sum (: :n := :0) inf
-          (inv (fact :n)))
-     := :e))
-(define fib-definition
-  (let ((fib (lambda x*
-               (ap0 '(mi "fib") (apply : x*)))))
-    (: (fib :n) :=
-       (choice
-        `((,:0 ,(: cm :n := :0))
-          (,:1 ,(: cm :n := :1))
-          (,(: (fib :n :- :1) :+
-               (fib :n :- :2))
-           ,(: cm :n :>= :2)))))))
-(define example0
-  (: (sum (: :k := :1) :n (:cub :k)) :=
-     (:sqr
-      (brac
-       (Mfrac (: :n i* (par0 (: :n :+ :1)))
-              :2)))))
-(define example1
-  (: (lim :x :0
-          (Mfrac (ap0 '(mi "sin") :x) :x))
-     := :1))
-(define example2
-  (: (sum (: :n := :1) inf
-          (inv (: :n i*
-                  (par0 (: :n :+ :1)))))
-     := :1))
-(define linear-equations
-  (let ((A11 (_ :A (: :1 cm :1)))
-        (A1n (_ :A (: :1 cm :n)))
-        (Am1 (_ :A (: :m cm :1)))
-        (Amn (_ :A (: :m cm :n)))
-        (x1 (_ :x :1))
-        (xn (_ :x :n))
-        (y1 (_ :y :1))
-        (ym (_ :y :m)))
-    (choice
-     (list
-      (list (: A11 i* x1) :+ ..c :+ (: A1n i* xn) := y1)
-      (list ..v $ $ $ ..v $ ..v)
-      (list (: Am1 i* x1) :+ ..c :+ (: Amn i* xn) := ym)))))
-(define euler-identity
-  (: (^ :e (: :i i* :pi)) :+ :1 := :0))
+(define comb-def
+  (&= (comb $n $k)
+      (~ (fact $n)
+         (i* (fact $k)
+             (fact (@ (&- $n $k)))))))
+(define comb-id
+  (&= (comb $m $n)
+      (&+ (comb (&- $m $1) (&- $n $1))
+          (comb (&- $m $1) $n))))
+(define binomial-id
+  (&= (^ (@ (&+ $x $y)) $n)
+      (sum (&= $k $0) $n
+           (i* (comb $n $k)
+               (^ $x (&- $n $k))
+               (^ $y $k)))))
+(define ex0
+  (&= (sum (&= $n $1) inf
+           (inv (i* $n (@ (&+ $n $1)))))
+      $1))
+(define ex1
+  (&= (sum (&= $k $1) $n (^ $k $2))
+      (~ (i* $n (@ (&+ $n $1))
+             (@ (&+ (i* $2 $n) $1)))
+         $6)))
+(define ex2
+  (&cm (&= (sum (&= $n $0) inf (^ $x $n))
+           (inv (&- $1 $x)))
+       (&< (ver0 $x) $1)))
+(define euler-id
+  (&= (&+ (^ $e (i* $i $pi)) $1) $0))
+(define ex3
+  (&= (sum (&= $k $1) $n (^ $k $3))
+      (^ (pare (sum (&= $k $1) $n $k))
+         $2)))
+(define e-def
+  (lim $n inf
+       (^ (pare (&+ $1 (inv $n)))
+          $n)))
+(define compute-e
+  (sum (&= $n $0) inf
+       (inv (fact $n))))
+(define cauchy-ineq
+  (&<=
+   (^ (@ (&+ (i* (_ $x $1) (_ $y $1))
+             ..c
+             (i* (_ $x $n) (_ $y $n))))
+      $2)
+   (i* (@ (&+ (_^ $x $1 $2) ..c (_^ $x $n $2)))
+       (@ (&+ (_^ $y $1 $2) ..c (_^ $y $n $2))))))
+(define pi-series
+  (&= (sum (&= $n $0) inf
+           (~ (^ (@ $-1) $n)
+              (&+ (i* $2 $n) $1)))
+      (~ $pi $4)))
+(define fib-def
+  (let ((fib (lambda (x)
+               (ap '(mi "fib") (@ x)))))
+    (&= (fib $n)
+        (choice
+         `((,$0 ,(: cm (&= $n $0)))
+           (,$1 ,(: cm (&= $n $1)))
+           (,(&+ (fib (&- $n $1))
+                 (fib (&- $n $2)))
+            ,(: cm (&>= $n $2))))))))
