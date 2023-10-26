d6c330b7c0591826eb01df06322dee551ce47c59
alpha-beta-eta <95096031+alpha-beta-eta@users.noreply.github.com>
2023-10-27 01:00:11 +0800

    Add files via upload

--- slt.rkt
+++ slt.rkt
@@ -10,34 +10,66 @@
 ;           |  ((text) <text-handler>)
 ;<handler> : <symbol> * <attr>* * <xml>* -> <xml>
 ;<text-handler> : <string> -> <string>
-(define ((T style*) xml)
-  (match xml
-    ((,tag ,attr* . ,xml*)
-     (cond
-       ((assq tag style*)
-        =>
-        (lambda (binding)
-          (match binding
-            ((,tag *preorder* ,handler)
-             (apply handler tag attr* xml*))
-            ((,tag ,handler)
-             (apply handler tag attr* (map (T style*) xml*))))))
-       ((assoc '(default) style*)
-        =>
-        (lambda (binding)
-          (match binding
-            (((default) *preorder* ,handler)
-             (apply handler tag attr* xml*))
-            (((default) ,handler)
-             (apply handler tag attr* (map (T style*) xml*))))))
-       (else
-        (error 'T "~s" tag))))
-    (,str
-     (guard (string? str))
-     (let ((binding (assoc '(text) style*)))
-       (if binding
-           ((cadr binding) str)
-           (error 'T "(text)"))))))
+;(define ((T style*) xml)
+;  (match xml
+;    ((,tag ,attr* . ,xml*)
+;     (cond
+;       ((assq tag style*)
+;        =>
+;        (lambda (binding)
+;          (match binding
+;            ((,tag *preorder* ,handler)
+;             (apply handler tag attr* xml*))
+;            ((,tag ,handler)
+;             (apply handler tag attr* (map (T style*) xml*))))))
+;       ((assoc '(default) style*)
+;        =>
+;        (lambda (binding)
+;          (match binding
+;            (((default) *preorder* ,handler)
+;             (apply handler tag attr* xml*))
+;            (((default) ,handler)
+;             (apply handler tag attr* (map (T style*) xml*))))))
+;       (else
+;        (error 'T "~s" tag))))
+;    (,str
+;     (guard (string? str))
+;     (let ((binding (assoc '(text) style*)))
+;       (if binding
+;           ((cadr binding) str)
+;           (error 'T "(text)"))))))
+(define (T style*)
+  (define default-binding
+    (cdr (assoc '(default) style*)))
+  (define default-preorder?
+    (eq? (car default-binding) '*preorder*))
+  (define default-handler
+    (if (procedure? (car default-binding))
+        (car default-binding)
+        (cadr default-binding)))
+  (define text-handler
+    (cadr (assoc '(text) style*)))
+  (define Style*
+    (filter (lambda (style) (symbol? (car style)))
+            style*))
+  (define (transform xml)
+    (match xml
+      ((,tag ,attr* . ,xml*)
+       (cond
+         ((assq tag Style*)
+          =>
+          (lambda (binding)
+            (match binding
+              ((,tag *preorder* ,handler)
+               (apply handler tag attr* xml*))
+              ((,tag ,handler)
+               (apply handler tag attr* (map transform xml*))))))
+         (else
+          (if default-preorder?
+              (apply default-handler tag attr* xml*)
+              (apply default-handler tag attr* (map transform xml*))))))
+      (,str (text-handler str))))
+  transform)
 (define (attr*-set attr* x v)
   (cond ((null? attr*)
          (list (list x v)))
