98cc3c7edc062d1b7238a2e1b6052c92623935f7
alpha-beta-eta <95096031+alpha-beta-eta@users.noreply.github.com>
2023-12-07 11:34:43 +0800

    Add files via upload
    Now in the <math> context you don't have to bracket text with <mtext>.

--- math-styles.rkt
+++ math-styles.rkt
@@ -5,13 +5,21 @@
   `(((default)
      *preorder*
      ,(lambda (tag attr* . xml*)
-        (cond ((eq? tag 'math) `(,tag ,attr* . ,xml*))
+        (cond ((eq? tag 'math) `(,tag ,attr* . ,(map Tx xml*)))
               ((memq tag '(merror mfrac mi mmultiscripts mn mo mover
                                   mpadded mphantom mroot mrow
                                   ms mspace msqrt mstyle msub msubsup
                                   msup mtable mtd mtext mtr munder
                                   munderover))
-               (Math `(,tag ,attr* . ,xml*)))
-              (else `(,tag ,attr* . ,(map Tm xml*))))))
-    ((text) ,(lambda (str) str))))
+               (Math (Tx `(,tag ,attr* . ,xml*))))
+              (else `(,tag ,attr* . ,(map Tm xml*))))))))
+;token elements: mtext mi mn mo mspace ms
+(define mtext-style*
+  `(((default)
+     *preorder*
+     ,(lambda (tag attr* . xml*)
+        (cond ((memq tag '(mtext mi mn mo mspace ms)) `(,tag ,attr* . ,xml*))
+              (else `(,tag ,attr* . ,(map Tx xml*))))))
+    ((text) ,(lambda (str) (% str)))))
 (define Tm (T math-style*))
+(define Tx (T mtext-style*))
